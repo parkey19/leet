47cf7a0f6993f7d1b8bb3f4ee244f7336a7a4561
SungJinPark <kkttqq@gmail.com>
2019-02-22 23:59:45 +0900

    SellStock2

--- src/main/java/classic/SellStock2.java
+++ src/main/java/classic/SellStock2.java
@@ -5,7 +5,8 @@ package classic;
  */
 public class SellStock2 {
     public static void main(String[] args) {
-        int[] prices = {7,5,4};
+//        int[] prices = {7,5,4};
+        int[] prices = {1,2,3,4,5};
         System.out.println(maxProfit(prices));
     }
 
@@ -14,10 +15,14 @@ public class SellStock2 {
         int profit = 0;
         int j = 1;
         for (int i = 0; i < prices.length-1; i++) {
-            if (prices[i] > prices[j+i]) {
+            if (prices[i] > prices[i+j]) {
                 continue;
             }
-
+            for (int k = i+j; k < prices.length-1; k++) {
+                if (prices[i] == prices[k]) {
+                    buy = prices[i];
+                }
+            }
         }
         return profit;
     }
