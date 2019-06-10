72241cff559ab6f4638466be3e7f99f566cd4c3a
SungJinPark <kkttqq@gmail.com>
2019-06-10 18:55:31 +0900

    진수 변환

--- src/main/java/basic/ch2/PrimeNumber.java
+++ src/main/java/basic/ch2/PrimeNumber.java
@@ -3,6 +3,10 @@ package basic.ch2;
 public class PrimeNumber {
 
     public static void main(String[] args) {
+        upgradePrime();
+    }
+
+    static void brutePrime(){
         int counter = 0;
 
         for (int n = 2; n < 10; n++) {
@@ -18,5 +22,32 @@ public class PrimeNumber {
             }
 
         }
+    }
+
+    static void upgradePrime(){
+        int counter = 0;
+        int ptr = 0;
+        int[] prime = new int[500];
+
+        prime[ptr++] = 2;
+
+        for (int n = 3; n <= 10; n+=2) {
+            int i;
+            for (i = 0; i < ptr; i++) {
+                counter++;
+                if (n % prime[i] == 0) {
+                    break;
+                }
+            }
+            if (ptr == i) {
+                prime[ptr++] = n;
+            }
+
+        }
+
+        for (int i = 0; i < ptr; i++) {
+            System.out.println(prime[i]);
+        }
+
     }
 }
