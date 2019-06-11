5d1851ace063f2531b0b3894c8f13136d2763567
SungJinPark <kkttqq@gmail.com>
2019-06-11 18:07:28 +0900

    소수

--- src/main/java/basic/ch2/PrimeNumber.java
+++ src/main/java/basic/ch2/PrimeNumber.java
@@ -4,7 +4,10 @@ public class PrimeNumber {
 
     public static void main(String[] args) {
         brutePrime();
+        System.out.println("---");
         upgradePrime();
+        System.out.println("---");
+        upgrade2Prime();
     }
 
     static void brutePrime() {
@@ -57,4 +60,40 @@ public class PrimeNumber {
         long e = System.nanoTime();
         System.out.println(e-s);
     }
+
+    static void upgrade2Prime(){
+        long s = System.nanoTime();
+        int counter = 0;
+        int ptr = 0;
+        int[] prime = new int[500];
+
+        prime[ptr++] = 2;
+        prime[ptr++] = 3;
+
+        for (int n = 5; n <= 1000; n+=2) {
+            boolean flag = false;
+            for (int i = 1; prime[i] * prime[i] <= n; i++) {
+                counter+=2;
+                if (n % prime[i] == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                prime[ptr++] = n;
+                counter++;
+            }
+
+        }
+
+        for (int i = 0; i < ptr; i++) {
+            System.out.println(prime[i]);
+        }
+
+        long e = System.nanoTime();
+        System.out.println(e-s);
+    }
+    //7325323
+    //5086222
+    //2836611
 }
