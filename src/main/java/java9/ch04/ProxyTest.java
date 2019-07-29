5a89db0740bdb1d4b9804e83a3b7bd98a677fde5
SungJinPark <kkttqq@gmail.com>
2019-07-29 15:06:37 +0900

    ch04 proxy

---
+++ src/main/java/java9/ch04/ProxyTest.java
@@ -0,0 +1,33 @@
+package java9.ch04;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+/**
+ * Created by parkey19 on 2019. 7. 29..
+ */
+public class ProxyTest {
+
+    public static Object[] createObj() {
+        Object[] values = new Object[1000];
+
+        for (int i = 0; i < values.length; i++) {
+            Object val = new Integer(i);
+            values[i] = Proxy.newProxyInstance(null, val.getClass().getInterfaces(),
+                    (Object proxy, Method m, Object[] margs) -> {
+                        System.out.println(val + "." + m.getName() + Arrays.toString(margs));
+                        return m.invoke(val, margs);
+                    }
+            );
+
+        }
+        return values;
+    }
+
+    public static void main(String[] args) {
+        Object[] obj = ProxyTest.createObj();
+
+        Arrays.binarySearch(obj, new Integer(500));
+    }
+}
