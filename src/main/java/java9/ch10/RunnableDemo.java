27931ff27dc91e13fc1994a51d6fe5ccd7595477
SungJinPark <kkttqq@gmail.com>
2019-08-20 18:43:16 +0900

    ch10 callable

---
+++ src/main/java/java9/ch10/RunnableDemo.java
@@ -0,0 +1,25 @@
+package java9.ch10;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by parkey19 on 2019. 8. 20..
+ */
+public class RunnableDemo {
+    public static void main(String[] args) {
+        Runnable hellos = () -> {
+            for (int i = 1; i <= 1000; i++)
+                System.out.println("Hello " + i);
+        };
+        Runnable goodbyes = () -> {
+            for (int i = 1; i <= 1000; i++)
+                System.out.println("Goodbye " + i);
+        };
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.execute(hellos);
+        executor.execute(goodbyes);
+        executor.shutdown();
+    }
+}
